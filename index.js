455847d7bd3377fb6701e6e1d2e9de0a394ef131
Edi Gal <edig@yad2.co.il>
2020-01-05 13:33:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const redis = require("redis");
+const process = require("process");
+
+const app = express();
+const redisClient = redis.createClient({
+  host: "redis-server",
+  port: 6379
+});
+redisClient.set("visits", 0);
+
+app.get("/", (req, res, next) => {
+  redisClient.get("visits", (err, visites) => {
+    if (err) {
+      res.send(`error:, ${err}`);
+    } else {
+      res.send(`Aloha visits: ${visites}`);
+      redisClient.set("visits", ++visites);
+    }
+  });
+});
+
+const { PORT = 8080 } = process.env;
+app.listen(PORT, console.log(`simple web is listen on port ${PORT}`));
